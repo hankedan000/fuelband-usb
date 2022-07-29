bf6e1a7cd2ff8abc88456e21fa9568b7f7fad814
hankedan000 <dany32412@gmail.com>
2022-07-28 20:21:54 -0400

    Moves bitfield methods to utils

--- nike/__init__.py
+++ nike/__init__.py
@@ -73,47 +73,6 @@ FUELBAND_STATUS_BITFIELDS = [
     {'mask' : 0x0000000000000100, 'name' : 'accel_present'}
 ]
 
-def get_shift(mask):
-    if mask == 0:
-        return 0
-    else:
-        shift = 0
-        while (mask & 0x1) == 0:
-            mask = mask >> 1
-            shift = shift + 1
-        return shift
-
-def print_bitfield_line(value, mask, name, n_bits=64):
-    masked_value = value & mask
-    i_mask = (1 << (n_bits - 1))
-    for b in range(n_bits):
-        if b > 0 and b % 4 == 0:
-            print(' ', end='')
-
-        if mask & i_mask:
-            if masked_value & i_mask:
-                print('1', end='')
-            else:
-                print('0', end='')
-        else:
-            print('.', end='')
-        i_mask = i_mask >> 1
-
-    shift = get_shift(mask)
-    shifted_value = masked_value >> shift
-    print(" : '%s' %d (0x%x)" % (name,shifted_value,shifted_value))
-
-def print_bitfield_rows(value, bitfield_def, n_bits=64, show_unknown=True):
-    if show_unknown:
-        unknown_mask = 0
-        for row in bitfield_def:
-            unknown_mask = unknown_mask | row['mask']
-        unknown_mask = unknown_mask ^ ((1 << n_bits) - 1)# invert the mask
-        bitfield_def = [{'mask' : unknown_mask, 'name' : 'reserved/unknown'}] + bitfield_def
-
-    for row in bitfield_def:
-        print_bitfield_line(value, row['mask'], row['name'], n_bits)
-
 class Fuelband(FuelbandBase):
     PID = 0x6565# Fuelband USB product id
 
@@ -272,7 +231,7 @@ class Fuelband(FuelbandBase):
     def printStatusBitfield(self, show_expected=False):
         status_word = int.from_bytes(self.status_bytes, 'big')
         print('status: 0x%016x (actual)' % status_word)
-        print_bitfield_rows(status_word, FUELBAND_STATUS_BITFIELDS, 64)
+        utils.print_bitfield_rows(status_word, FUELBAND_STATUS_BITFIELDS, 64)
 
         if not show_expected:
             return
@@ -282,7 +241,7 @@ class Fuelband(FuelbandBase):
         # got this from a log dump from the fuelband itself
         EXPECT_STATUS = 0x00CF3F5707FF0700
         print('status: 0x%016x (expect)' % EXPECT_STATUS)
-        print_bitfield_rows(EXPECT_STATUS, FUELBAND_STATUS_BITFIELDS, 64)
+        utils.print_bitfield_rows(EXPECT_STATUS, FUELBAND_STATUS_BITFIELDS, 64)
 
     def printStatus(self):
         self.doVersion()
--- nike/utils.py
+++ nike/utils.py
@@ -11,7 +11,6 @@ def to_hex(buf):
         t_buf = t_buf + '%02x' % i
     return t_buf
 
-
 def print_ascii(buf, newline=False):
     for i in buf:
         print('%c' % i, end='')
@@ -29,3 +28,44 @@ def intFromLittleEndian(buf):
     for i in range(len(buf)):
         t_num = t_num | (buf[len(buf) - i - 1] << (i * 8))
     return t_num
+
+def get_shift(mask):
+    if mask == 0:
+        return 0
+    else:
+        shift = 0
+        while (mask & 0x1) == 0:
+            mask = mask >> 1
+            shift = shift + 1
+        return shift
+
+def print_bitfield_line(value, mask, name, n_bits=64):
+    masked_value = value & mask
+    i_mask = (1 << (n_bits - 1))
+    for b in range(n_bits):
+        if b > 0 and b % 4 == 0:
+            print(' ', end='')
+
+        if mask & i_mask:
+            if masked_value & i_mask:
+                print('1', end='')
+            else:
+                print('0', end='')
+        else:
+            print('.', end='')
+        i_mask = i_mask >> 1
+
+    shift = get_shift(mask)
+    shifted_value = masked_value >> shift
+    print(" : '%s' %d (0x%x)" % (name,shifted_value,shifted_value))
+
+def print_bitfield_rows(value, bitfield_def, n_bits=64, show_unknown=True):
+    if show_unknown:
+        unknown_mask = 0
+        for row in bitfield_def:
+            unknown_mask = unknown_mask | row['mask']
+        unknown_mask = unknown_mask ^ ((1 << n_bits) - 1)# invert the mask
+        bitfield_def = [{'mask' : unknown_mask, 'name' : 'reserved/unknown'}] + bitfield_def
+
+    for row in bitfield_def:
+        print_bitfield_line(value, row['mask'], row['name'], n_bits)
